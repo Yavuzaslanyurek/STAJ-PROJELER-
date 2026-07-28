96950755ce2aee011dd63997f27c5504e6c623b8
Yavuzaslanyurek <yavuzaslanyurek14@gmail.com>
2026-07-28 13:11:50 +0300

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Yatırım.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
